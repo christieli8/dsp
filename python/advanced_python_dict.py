55cbe196b7ad8aecf4386b1b8c7ec322cc09d194
Christie Li <christiefloral121@gmail.com>
2016-06-17 23:38:20 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,51 @@
-PLACE YOUR CODE HERE
+import csv
+
+faculty_dict = {}
+with open("/Users/christieli/Desktop/pre-work/dsp/python/faculty.csv", "rb") as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    name = row["name"]
+    tokens = name.split()
+
+    last_name = tokens[-1]
+    if last_name not in faculty_dict:
+      faculty_dict[last_name] = []
+    
+    faculty_dict[last_name].append([
+      row[" degree"].strip(),
+      row[" title"].strip(),
+      row[" email"].strip()
+    ])
+keys = faculty_dict.keys()
+for key in keys[:3]:
+  print key, faculty_dict[key]
+print
+
+professor_dict = {}
+with open("/Users/christieli/Desktop/pre-work/dsp/python/faculty.csv", "rb") as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    name = row["name"]
+    tokens = name.split()
+
+    first_name = tokens[0]
+    last_name = tokens[-1]
+    key = (first_name, last_name)
+    if key not in faculty_dict:
+      professor_dict[key] = []
+    
+    professor_dict[key].append([
+      row[" degree"].strip(),
+      row[" title"].strip(),
+      row[" email"].strip()
+    ])
+keys = professor_dict.keys()
+for key in keys[:3]:
+  print key, professor_dict[key]
+print
+
+keys = professor_dict.keys()
+keys.sort(key=lambda name: name[1])
+for key in keys[:3]:
+  print key, professor_dict[key]
+print
