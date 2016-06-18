33c2c840b0ba42939a29f1b54b0415ba701556ff
Christie Li <christiefloral121@gmail.com>
2016-06-17 23:27:29 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,9 @@
-PLACE YOUR CODE HERE
+import csv
+
+outfile = open("/Users/christieli/Desktop/pre-work/emails.csv", "w")
+with open("/Users/christieli/Desktop/pre-work/dsp/python/faculty.csv", "rb") as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    email = row[" email"]
+    outfile.write(email + "\n")
+outfile.close()
